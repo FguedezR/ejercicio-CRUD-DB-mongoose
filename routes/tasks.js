ed6962844ea1dafe08e2fb8a753cb4dd41405942
Fernando <fernanguedezr@gmail.com>
2026-04-02 13:24:59 +0200

    Add new files

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task");
 
-// CREATE
+// CREATE crea una tarea
 router.post("/create", async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -13,3 +13,28 @@ router.post("/create", async (req, res) => {
     res.status(400).send({ message: "Error al crear la tarea".error });
   }
 });
+
+// READ obtiene todas las tareas
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send({ message: "Error al obtener las tareas" });
+  }
+});
+
+// READ obtener tarea por id
+
+router.get("/id/:_id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params._id);
+    if (!task) return res.status(404).send({ message: "Tarea no encontrada" });
+    res.send(task);
+  } catch (error) {
+    res.status(500).send({ message: "ID no válido" });
+  }
+});
+
+// UPDATE marcar como completada
