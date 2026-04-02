6f92b29424382983674eb8a27ca2d4010a37ba49
Fernando <fernanguedezr@gmail.com>
2026-04-02 13:15:06 +0200

    Added tasks.js create route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,1 +1,15 @@
-// conexión a mongodb
+// lógica de rutas y controladores donde usaremos Router de Express
+
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/Task");
+
+// CREATE
+router.post("/create", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send({ message: "Error al crear la tarea".error });
+  }
+});
